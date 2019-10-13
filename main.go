8092854b527965ca40c2c8d393ea9b2e9a04051f
Yoshiharu <ysh86@users.noreply.github.com>
2019-10-13 17:36:56 +0900

    conv RGBM to RGB

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"image"
 	"image/png"
-	_ "github.com/spakin/netpbm"
+	"os"
+
+	"github.com/spakin/netpbm"
 )
 
 func main() {
@@ -15,10 +16,26 @@ func main() {
 	}
 	defer fin.Close()
 
-	img, format, err := image.Decode(fin)
+	i, format, err := image.Decode(fin)
 	fmt.Printf("Input format: %v\n", format)
+	fmt.Printf("Image: %v, %#v\n", i.Bounds(), i.ColorModel())
 	if err != nil {
 		panic(err)
+	}
+
+	// conv netpbm.npcolor.RGBM to RGBA
+	var img *image.RGBA
+	switch i := i.(type) {
+	case *netpbm.RGBM:
+		img = image.NewRGBA(i.Bounds())
+		for p := 0; p < len(img.Pix) / 4; p++ {
+			img.Pix[p*4+0] = i.Pix[p*3+0]
+			img.Pix[p*4+1] = i.Pix[p*3+1]
+			img.Pix[p*4+2] = i.Pix[p*3+2]
+			img.Pix[p*4+3] = 0xff
+		}
+	default:
+		panic(fmt.Errorf("unknown format %T", i))
 	}
 
 	fout, err := os.Create(os.Args[2])
